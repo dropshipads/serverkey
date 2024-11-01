df7f22a2232faf3a038daded854ea35f98464a9b
Duc Th1nh <droppshipads@gmail.com>
2024-11-02 01:00:12 +0700

    1111111

--- index.js
+++ index.js
@@ -12,10 +12,20 @@ if (!secretKey) {
   throw new Error("SECRET_KEY is not defined. Make sure it's set in the environment variables.");
 }
 
-// Khởi tạo Firebase Admin SDK
-const serviceAccount = require('./firebase-service-account.json');
+// Khởi tạo Firebase Admin SDK bằng biến môi trường
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    type: "service_account",
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // Xử lý xuống dòng
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+  })
 });
 
 const db = admin.firestore();
