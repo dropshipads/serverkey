6ee7fc777751b111b15b09045e6191ce2c7bf196
Duc Th1nh <droppshipads@gmail.com>
2024-11-03 15:04:47 +0700

    1111111111111

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.get("/check", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "quanly.html"));
+});
 // Endpoint để tạo token JWT
 app.get("/generate-key", async (req, res) => {
   const { year, month, day, hour, minute } = req.query;
